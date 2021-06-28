fe6fd4c97c178b34ebb46bc042152141a4603e20
Tanguy-jpg <tanguycarnot@gmail.com>
2021-06-28 18:38:58 +0100

    rectification*

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function temp(){
             url: `http://api.openweathermap.org/data/2.5/forecast?q=${ville}&appid=e97d6758af72995c3e16f0f40300d7b3` ,
             type: "GET",
             success: function (result){
-                document.querySelector("article#one").innerHTML= `à ${ville} il fait <br> ${result.list[0].main.temp-273.15}°`
+                document.querySelector("article#one").innerHTML= `City: ${ville.toUpperCase()} <br><br>temperature: ${result.list[0].main.temp-273.15}°`
             },
             error: function(error){
                 document.querySelector("article#one").innerHTML= "veuillez entrer une ville valide";
