e921642b74c1248aadd6e432c523662c22a1d08e
Tanguy-jpg <tanguycarnot@gmail.com>
2021-06-28 19:22:13 +0100

    ajout conditions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 function temp(){
-    let ville = prompt("Ville");
+    let ville = prompt("Place: ");
 
     $(document).ready(function(){
         $.ajax({
             url: `http://api.openweathermap.org/data/2.5/forecast?q=${ville}&appid=e97d6758af72995c3e16f0f40300d7b3` ,
             type: "GET",
             success: function (result){
-                document.querySelector("article#one").innerHTML= `City: ${ville.toUpperCase()} <br><br>temperature: ${result.list[0].main.temp-273.15}°`
-            },
+                document.querySelector("article#one").innerHTML= `Place: ${ville.toUpperCase()} <br><br>temperature: ${Math.round((result.list[0].main.temp-273.15)*100)/100}° <br><br><br><br><input type='button' value='Change Place' onclick='temp()'>`;
+
+                if (result.list[0].main.temp-273.15 <= 0){
+                    alert('très froid');
+                } else if (result.list[0].main.temp-273.15 <= 10){
+                    alert('froid');
+                } else if (result.list[0].main.temp-273.15 <= 19){
+                    alert('moyen');
+                } else if(result.list[0].main.temp-273.15 <= 30){
+                    alert('good');
+                } else if(result.list[0].main.temp-273.15 >= 30){
+                    alert('chaud');
+                }      
+                
+                },
             error: function(error){
-                document.querySelector("article#one").innerHTML= "veuillez entrer une ville valide";
+                document.querySelector("article#one").innerHTML= "veuillez entrer une ville valide <br><br><br><br> <input type='button' value='relancer' onclick='temp()'>";
             }
         })
     })
