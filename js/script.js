45632c8696453e5a863dbfd4aab443212d56fd5d
Tanguy-jpg <tanguycarnot@gmail.com>
2021-06-28 23:10:59 +0100

    terminé

--- js/script.js
+++ js/script.js
@@ -1,28 +1,32 @@
-function temp(){
+function temp(n){
     let ville = prompt("Place: ");
 
     $(document).ready(function(){
         $.ajax({
-            url: `http://api.openweathermap.org/data/2.5/forecast?q=${ville}&appid=e97d6758af72995c3e16f0f40300d7b3` ,
+            url: `http://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=e97d6758af72995c3e16f0f40300d7b3` ,
             type: "GET",
-            success: function (result){
-                document.querySelector("article#one").innerHTML= `Place: ${ville.toUpperCase()} <br><br>temperature: ${Math.round((result.list[0].main.temp-273.15)*100)/100}° <br><br><br><br><input type='button' value='Change Place' onclick='temp()'>`;
 
-                if (result.list[0].main.temp-273.15 <= 0){
-                    alert('très froid');
-                } else if (result.list[0].main.temp-273.15 <= 10){
-                    alert('froid');
-                } else if (result.list[0].main.temp-273.15 <= 19){
-                    alert('moyen');
-                } else if(result.list[0].main.temp-273.15 <= 30){
-                    alert('good');
-                } else if(result.list[0].main.temp-273.15 >= 30){
-                    alert('chaud');
-                }      
+            success: function (result){
+                document.querySelector(n+' p').style="color:black;"
+                let temperature = Math.round((result.main.temp-273.15)*100)/100;
+
+                document.querySelector(n+' p').innerHTML= `Place: ${ville.toUpperCase()} <br><br> <p>temperature: <strong>${temperature}°</strong></p> `;
+
+                if(temperature >= 29){
+                    document.querySelector(n+' strong').style="color:#c50d0d;" //red
+                }else if(temperature>=20){
+                    document.querySelector(n+' strong').style="color:#0feb0f;" //green
+                }else if(temperature>=10){
+                    document.querySelector(n+' strong').style="color:#dceb0f;" //yellow
+                }else if(temperature>=0){
+                    document.querySelector(n+' strong').style="color:#0f8ceb;" //blue
+                }
                 
-                },
+            },
+
             error: function(error){
-                document.querySelector("article#one").innerHTML= "veuillez entrer une ville valide <br><br><br><br> <input type='button' value='relancer' onclick='temp()'>";
+                document.querySelector(n+' p').style="color:#c50d0d;"
+                document.querySelector(n+' p').innerHTML= "invalid entry";
             }
         })
     })
