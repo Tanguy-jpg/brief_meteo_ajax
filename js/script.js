37cf2b04d7a6fbecc71c429bed7081d80b2be35b
Tanguy-jpg <tanguycarnot@gmail.com>
2021-07-04 00:18:58 +0100

    Graph terminés

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const close = (n)=>(document.querySelector(n).style="display:none");
+const close = (n)=>(document.querySelector(n).style="display:none", myChart.destroy());
+
 
 function temp(n){
     let ville = prompt("Place: ");
@@ -56,6 +57,8 @@ let placeFour;
 
 
 function graph(n){
+    document.querySelector('#chart').style="";
+    
     n==='#one'? ville=placeOne:'';
     n==='#two'? ville=placeTwo:'';
     n==='#three'? ville=placeThree:'';
@@ -70,81 +73,79 @@ function graph(n){
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
                     chartInfo = [];
                     chartInfo.push(
-                        [[result.list[0].dt_txt,result.list[0].main.temp],
-                        [result.list[1].dt_txt,result.list[1].main.temp],
-                        [result.list[2].dt_txt,result.list[2].main.temp],
-                        [result.list[4].dt_txt,result.list[4].main.temp]],
+                        [[result.list[0].dt_txt.split(' '),result.list[0].main.temp],
+                        [result.list[1].dt_txt.split(' '),result.list[1].main.temp],
+                        [result.list[2].dt_txt.split(' '),result.list[2].main.temp],
+                        [result.list[4].dt_txt.split(' '),result.list[4].main.temp]],
 
-                        [[result.list[8].dt_txt,result.list[8].main.temp],
-                        [result.list[9].dt_txt,result.list[9].main.temp],
-                        [result.list[10].dt_txt,result.list[10].main.temp],
-                        [result.list[12].dt_txt,result.list[12].main.temp]],
+                        [[result.list[8].dt_txt.split(' '),result.list[8].main.temp],
+                        [result.list[9].dt_txt.split(' '),result.list[9].main.temp],
+                        [result.list[10].dt_txt.split(' '),result.list[10].main.temp],
+                        [result.list[12].dt_txt.split(' '),result.list[12].main.temp]],
 
-                        [[result.list[16].dt_txt,result.list[16].main.temp],
-                        [result.list[17].dt_txt,result.list[17].main.temp],
-                        [result.list[18].dt_txt,result.list[18].main.temp],
-                        [result.list[20].dt_txt,result.list[20].main.temp]],
+                        [[result.list[16].dt_txt.split(' '),result.list[16].main.temp],
+                        [result.list[17].dt_txt.split(' '),result.list[17].main.temp],
+                        [result.list[18].dt_txt.split(' '),result.list[18].main.temp],
+                        [result.list[20].dt_txt.split(' '),result.list[20].main.temp]],
 
-                        [[result.list[24].dt_txt,result.list[24].main.temp],
-                        [result.list[25].dt_txt,result.list[25].main.temp],
-                        [result.list[26].dt_txt,result.list[26].main.temp],
-                        [result.list[28].dt_txt,result.list[28].main.temp]],
+                        [[result.list[24].dt_txt.split(' '),result.list[24].main.temp],
+                        [result.list[25].dt_txt.split(' '),result.list[25].main.temp],
+                        [result.list[26].dt_txt.split(' '),result.list[26].main.temp],
+                        [result.list[28].dt_txt.split(' '),result.list[28].main.temp]],
 
-                        [[result.list[32].dt_txt,result.list[32].main.temp],
-                        [result.list[33].dt_txt,result.list[33].main.temp],
-                        [result.list[34].dt_txt,result.list[34].main.temp],
-                        [result.list[36].dt_txt,result.list[36].main.temp]]
+                        [[result.list[32].dt_txt.split(' '),result.list[32].main.temp],
+                        [result.list[33].dt_txt.split(' '),result.list[33].main.temp],
+                        [result.list[34].dt_txt.split(' '),result.list[34].main.temp],
+                        [result.list[36].dt_txt.split(' '),result.list[36].main.temp]]
                         );
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-                            const labels = [
-                                "Aujourd'hui",
-                                'Demain',
-                                'Après-demain',
-                                'Dans 3 jours',
-                                'Dans 4 jours',
-                            ];
-                            const data = {
-                                labels: labels,
-                                datasets: [
-                                {
-                                label: chartInfo[0][0][0],
-                                backgroundColor: 'rgb(94 43 255)',
-                                borderColor: 'rgb(255, 99, 132)',
-                                data: [0, 10, 5, 2, 20, 30, 45],
-                                },
-                                {
-                                    label: 'Midi',
-                                    backgroundColor: 'rgb(192 76 253)',
+                                const labels = [
+                                    chartInfo[0][0][0][0], //date d'aujourd'hui
+                                    chartInfo[1][0][0][0], //date du lendemain
+                                    chartInfo[2][0][0][0], //ainsi de suite 
+                                    chartInfo[3][0][0][0],
+                                    chartInfo[4][0][0][0],
+                                ];
+                                const data = {
+                                    labels: labels,
+                                    datasets: [
+                                    {
+                                    label: chartInfo[0][0][0][1], //heure jour 1
+                                    backgroundColor: 'rgb(94 43 255)',
                                     borderColor: 'rgb(255, 99, 132)',
-                                    data: [0, 10, 5, 2, 20, 30, 45],
-                                },
-                                {
-                                    label: 'Après-midi',
-                                    backgroundColor: 'rgb(252 109 171)',
-                                    borderColor: 'rgb(255, 99, 132)',
-                                    data: [5, 10, 5, 2, 20, 30, 45],
-                                },
-                                {
-                                    label: 'Soir',
-                                    backgroundColor: 'rgb(247 246 197)',
-                                    borderColor: 'rgb(255, 99, 132)',
-                                    data: [0, 10, 5, 2, 20, 30, 45],
-                                },
-                                ]
-                            };
-                            
-                            const config = {
-                                type: 'bar',
-                                data,
-                                options: {}
-                            };
-
-                            var myChart = new Chart(
-                                document.getElementById('myChart'),
-                                config
-                            );
-////////////////////////////////////////////////////////////////////////////////////////////////////      
-                    document.querySelector('#chart').style="";
+                                    data: [(chartInfo[0][0][1]),(chartInfo[1][0][1]),(chartInfo[2][0][1]),(chartInfo[3][0][1]),(chartInfo[4][0][1])],
+                                    },
+                                    {
+                                        label: chartInfo[0][1][0][1], //heure jour 2
+                                        backgroundColor: 'rgb(192 76 253)',
+                                        borderColor: 'rgb(255, 99, 132)',
+                                        data: [(chartInfo[0][1][1]),(chartInfo[1][1][1]),(chartInfo[2][1][1]),(chartInfo[3][1][1]),(chartInfo[4][1][1])],
+                                    },
+                                    {
+                                        label: chartInfo[0][2][0][1], //heure jour 3
+                                        backgroundColor: 'rgb(252 109 171)',
+                                        borderColor: 'rgb(255, 99, 132)',
+                                        data: [(chartInfo[0][2][1]),(chartInfo[1][2][1]),(chartInfo[2][2][1]),(chartInfo[3][2][1]),(chartInfo[4][2][1])],
+                                    },
+                                    {
+                                        label: chartInfo[0][3][0][1], //heure jour 4
+                                        backgroundColor: 'rgb(247 246 197)',
+                                        borderColor: 'rgb(255, 99, 132)',
+                                        data: [(chartInfo[0][3][1]),(chartInfo[1][3][1]),(chartInfo[2][3][1]),(chartInfo[3][3][1]),(chartInfo[4][3][1])],
+                                    },
+                                    ]
+                                };
+                                
+                                const config = {
+                                    type: 'bar',
+                                    data,
+                                    options: {}
+                                };  
+                                window.myChart = new Chart(
+                                    document.getElementById('myChart'),
+                                    config
+                            )
+////////////////////////////////////////////////////////////////////////////////////////////////////
                         
             },
             error: function(error){
@@ -154,4 +155,3 @@ function graph(n){
     })
 }
 let chartInfo = [];
-
